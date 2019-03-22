29b4b86496cf74f70308d3045c160cee8cf6445e
Viacheslav Ocheretnyi <viacheslav.ocheretnyi@sap.com>
2019-03-22 17:04:18 +0100

    Add project

---
+++ cmake-build-release/CMakeFiles/lcs_openmp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lcs_openmp.dir/main.cpp.o"
+  "lcs_openmp.pdb"
+  "lcs_openmp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lcs_openmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
